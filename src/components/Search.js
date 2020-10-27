82d7ed4cb4b4b2801c6c7fbdbd45459a0fef95d8
ryan-puffer <ryanpufferdev@gmail.com>
2020-10-27 13:27:57 -0500

    add axios request in search component

---
+++ src/components/Search.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Search = () => {
+	const [term, setTerm] = useState('');
+	const [results, setResults] = useState([]);
+
+	console.log(results);
+
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: term,
+				},
+			});
+			setResults(data.query.search);
+		};
+		if (term) {
+			search();
+		}
+	}, [term]);
+
+	return (
+		<div className='ui form'>
+			<div className='field'>
+				<label>Enter Search Term</label>
+				<input
+					className='input'
+					value={term}
+					onChange={(e) => setTerm(e.target.value)}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Search;
